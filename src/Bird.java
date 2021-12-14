e0d68b5419244d42c2ae98089fe76764bcdd2dfc
Ben Donaghy <bendonaghy321@gmail.com>
2021-12-14 23:03:23 +0000

    created Bird class

---
+++ src/Bird.java
@@ -0,0 +1,27 @@
+public class Bird
+{
+
+    protected String name;
+    protected String colour;
+    protected Integer maxSpeed;
+
+    public Bird(String name, String colour, Integer maxSpeed)
+    {
+        this.name = name;
+        this.colour = colour;
+        this.maxSpeed = maxSpeed;
+    }
+
+    public void speak() {
+        System.out.println(name + " wants a cracker!");
+    }
+
+    public void fly()
+    {
+        System.out.println("I'm " + name + " and I can fly at " + maxSpeed + " km/h!");
+    }
+
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        Bird myBird = new Bird("Tweety", "Yellow", 25);
+        myBird.speak();
+        myBird.fly();
+    }
+}
